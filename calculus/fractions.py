3888a3461036f6bf0878e58bb312248b5d76cde0
Benjamin <benjamin.boboul@imt-lille-douai.org>
2020-02-02 23:11:53 +0100

    Add fractions comparison

---
+++ calculus/fractions.py
@@ -0,0 +1,14 @@
+from fractions import Fraction
+from timeit import timeit
+
+def python_fraction():
+  return Fraction(22, 7)
+  
+timeit("python_fraction()", setup="from __main__ import python_fraction", number=1000)
+# 0.002394800000004693
+
+def frac_operator():
+  return 22/7
+
+timeit("frac_operator()", setup="from __main__ import frac_operator", number=1000)
+# 0.00010830800000860563
