b62c575af40ef5ba9f6eb0d64f757c1544913431
Benjamin <benjamin.boboul@imt-lille-douai.org>
2020-02-02 23:46:12 +0100

    Update string.py

--- formatting/string.py
+++ formatting/string.py
@@ -1,5 +1,5 @@
 from timeit import timeit
-
+from shlex import join as shjoin
 
 def toIdentifier(_key: str) -> str:
     from re import split
@@ -13,3 +13,16 @@ my_dict = "@branch-rate"
 
 timeit("toIdentifier(my_dict)", setup="from __main__ import toIdentifier, my_dict", number=1000)
 # 0.007201906002592295
+
+
+def string_join():
+    return ''.join(["this", "is", "a", "string", "jointure", "comparison"])
+
+timeit("string_join()", setup="from __main__ import string_join", number=1000)
+# 0.0005619599999135971
+
+def shlex_join():
+    return shjoin(["this", "is", "a", "string", "jointure", "comparison"])
+
+timeit("shlex_join()", setup="from __main__ import shlex_join", number=1000)
+# Don't have installed python 3.8 for now.
